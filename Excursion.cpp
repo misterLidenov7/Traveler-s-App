ead2af2b57d250b82bf2c3c11d452ec8e7266f9d
misterLidenov7 <73595599+misterLidenov7@users.noreply.github.com>
2021-05-31 15:15:05 +0300

    Add files via upload
    нов клас Excursion , в който се съхраняват данните за дадена ексурзия с дата на идване и тръгване , коменар  и името на дестинацията

---
+++ Excursion.cpp
@@ -0,0 +1,52 @@
+#include "Excursion.hpp"
+
+Excursion::Excursion()
+{
+    start = TimePeriod();
+    end = TimePeriod(1999, 1, 2);
+    dest  = Destination();
+    comm  = Comment();
+}
+
+Excursion::Excursion(const TimePeriod _start, const TimePeriod _end, const Destination _dest, const Comment _comm):
+    start(_start), end(_end) , dest(_dest), comm(_comm)
+{}
+
+TimePeriod Excursion::startGetter() const
+{
+    return start;
+}
+
+TimePeriod Excursion::endGetter() const
+{
+    return end;
+}
+
+Destination Excursion::destGetter() const
+{
+    return dest; }
+
+Comment Excursion::getComment() const
+{
+    return comm;
+}
+
+std::ostream& operator<<(std::ostream& out, const Excursion& excur)
+{
+    out << "Destination : " << excur.destGetter().getCity() << ", "
+        << excur.destGetter().getCountry();
+
+    if(excur.startGetter() < excur.endGetter())
+    {
+        out << "\nArrive date : " << excur.startGetter()
+            << "\nEnd date : " << excur.endGetter();
+    }
+    else
+    {
+        out << "\nArrive date must be before end date !\n";
+    }
+
+    out << "\nComment : " << excur.getComment().commentGetter() << "\n";
+
+    return out;
+}
---
+++ Excursion.hpp
@@ -0,0 +1,31 @@
+#ifndef EXCURSION_HPP
+#define EXCURSION_HPP
+
+#include "TimePeriod.hpp"
+#include "Vector.hpp"
+#include "Destination.hpp"
+#include "Comment.hpp"
+
+class Excursion{
+
+private:
+    TimePeriod start;
+    TimePeriod end;
+    Destination dest;
+    Comment comm;
+
+public:
+    Excursion();
+    Excursion(const TimePeriod _start, const TimePeriod _end, const Destination _dest, const Comment _comm);
+
+    TimePeriod startGetter() const;
+    TimePeriod endGetter() const;
+
+    Destination destGetter() const;
+
+    Comment getComment() const;
+
+    friend std::ostream& operator<<(std::ostream& out, const Excursion& excur);
+};
+
+#endif // !EXCURSION_HPP
